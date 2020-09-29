d1dbcd3bc14cca71c45789ff4781d14816177235
Jaeiya <exanix@gmail.com>
2020-09-28 17:16:04 -0700

    Command{}: Add default help method

--- command.ts
+++ command.ts
@@ -14,6 +14,8 @@ export abstract class Command {
 
   protected abstract _instruction(...args: string[]): void;
 
+  protected abstract _help(): void;
+
 
   exec(admin = false, ...args: string[]) {
     const msg = this.bot.curMsg;
@@ -22,6 +24,17 @@ export abstract class Command {
         `Sorry <@${msg.member?.id}>, you do not have the necessary \
         permissions use this command.`
       );
+    }
+    if (args[0] == 'help') {
+      if (!this._help) {
+        return this.bot.sendHighMsg(
+          'Jaeiya is a bad boy!! :face_with_symbols_over_mouth:\n' +
+          'He forgot to add a description to the help command. ' +
+          '\n\n**Make sure you yell at him thoroughly for me!**.',
+          'Command Has No Help'
+        );
+      }
+      return this._help();
     }
     this._instruction(...args);
   }
