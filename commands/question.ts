d9c4e40b44bd8f8515d0d1cec2bf6faa6b51cbba
Jaeiya <exanix@gmail.com>
2020-09-30 18:43:12 -0700

    cmd(qstn): Questions and URLs now use default code path.
    Entering either a question or URL is the most natural
    outcome of the question command. More specific
    commands will make more sense in light of this change.

--- commands/question.ts
+++ commands/question.ts
@@ -17,31 +17,19 @@ class QuestionCmd extends Command {
 
 
 
-  async _instruction(urlOrCmd: string, ...args: string[]) {
-    if (this._isCommand(urlOrCmd, ...args)) return
+  async _instruction(arg1: string, ...args: string[]) {
+    if (this._isCommand(arg1, ...args)) return
     ;
-    if (args.length) return this.bot.sendMedMsg(
-      `:thinking: I tried really hard, but I don't understand what you want.`
-    );
-    if (!this._urlEx.test(urlOrCmd)) return this.bot.sendMedMsg(
-      'Invalid URL for question parsing.'
-    );
-    const [err, doc] = await this._getQuestionDoc(urlOrCmd);
-    if (err) {
-      this.bot.sendHighMsg(
-        `An Error occurred while interacting with the URL:\n\`${err}\``
+    if (await this._isProcessingURL(arg1)) return;
+    if (!this._isFindingQuestion([arg1, ...args].join(' '))) {
+      this.bot.sendMedMsg(
+        'You entered an **Invalid** `URL` or `Question`'
       );
     }
-    this.bot.curMsg.delete();
-    this._parseQuestion(doc);
   }
 
 
   private _isCommand(cmd: string, ...args: string[]) {
-    if (cmd == 'find') {
-      this._findQuestion(args.join(' ').toLowerCase());
-      return true;
-    }
     if (cmd == 'list') {
       this._list();
       return true;
@@ -50,19 +38,33 @@ class QuestionCmd extends Command {
   }
 
 
-  private _findQuestion(question: string) {
-    const item = this.bot.sai.ask(question);
-    if (item == RepErrorCode.Question) {
-      return this.bot.sendMedMsg(
-        `That's an invalid question. :thinking: Did you ask it correctly?`
+  private async _isProcessingURL(url: string) {
+    if (!this._urlEx.test(url)) return false
+    ;
+    const [err, doc] = await this._getQuestionDoc(url);
+    this.bot.curMsg.delete()
+    ;
+    if (err) {
+      return !this.bot.sendHighMsg(
+        `An Error occurred while interacting with the URL:\n\`${err}\``
       );
     }
+    this._parseQuestion(doc);
+    return true;
+  }
+
+
+  private _isFindingQuestion(question: string) {
+    const item = this.bot.sai.ask(question);
+    if (item == RepErrorCode.Question) return false
+    ;
     if (!item) {
-      return this.bot.sendMedMsg(
-        `Sorry, I couldn't find that question in my knowledgebase.`
+      return !this.bot.sendMedMsg(
+        `Sorry, I couldn't find that question in my database.`
       );
     }
     this._sendQuestionDetails(item);
+    return true;
   }
 
 
