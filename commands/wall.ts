fa3d8c546bf486bdd3fc5f52b671cf4bfaf9f4f2
Jaeiya <exanix@gmail.com>
2020-10-04 17:25:37 -0700

    cmd(wall): Implement _help()

--- commands/wall.ts
+++ commands/wall.ts
@@ -7,6 +7,20 @@ class WallCmd extends Command {
     super(['wall'], Bot.Role.Everyone);
   }
 
+  _help() {
+    this.bot.sendLowMsg(
+`**Aliases**
+\`\`\`${this.aliases.join(', ')}\`\`\`
+**Wall**
+Sends a \`<size>\` amount of invisible lines, which when
+large enough, creates the illusion of an invisible wall
+of text. This is useful for giving the illusion of a *blank
+channel*, *hiding off-topic content*, or *just having fun*.
+\`\`\`;wall <size>\`\`\`
+${this.helpFooter}`,
+      'Wall Command Help'
+    );
+  }
 
   _instruction(size: string) {
     let lines = +size;
