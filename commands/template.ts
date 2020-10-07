fdfff9240674a76947c2bbf3c227e31e832b3335
Jaeiya <exanix@gmail.com>
2020-10-06 17:10:12 -0700

    Update command template

--- commands/template.ts
+++ commands/template.ts
@@ -1,13 +1,24 @@
 import Bot from '../bot';
 import { Command } from '../command';
 
+
+
+
 class TemplateCommand extends Command {
+
+  get help() {
+    return '';
+  }
+
+
   constructor(public bot: Bot) {
     super(['name'], Bot.Role.Everyone);
   }
+
+
   _instruction() {
     // Implementation goes here
   }
-}
 
+}
 export = TemplateCommand;
