cc410c101baccc2358d7fb51f65b2f5e63a44c26
Jaeiya <exanix@gmail.com>
2020-10-08 19:03:42 -0700

    cmd(ping): Implement Strings namespace

--- commands/ping.ts
+++ commands/ping.ts
@@ -3,15 +3,7 @@ import { Command } from '../command';
 
 class PingCmd extends Command {
 
-  get help() {
-    return (
-`**Pinging**
-Returns the milliseconds it takes for the bot to
-communicate with the Discord API. It requires no
-arguments.
-\`\`\`;ping\`\`\``
-    );
-  }
+  get help() { return Strings.help(); }
 
   constructor(bot: Bot) {
     super(['ping', 'p'], Bot.Role.Admin, bot);
@@ -20,8 +12,23 @@ arguments.
 
   _instructions() {
     const sock = this._bot.curMsg.client.ws;
-    this._bot.sendLowMsg(`\u2002:clock3: ${sock.ping}ms`);
+    this._bot.sendLowMsg(Strings.ping(sock.ping));
   }
 
+}
+
+
+namespace Strings {
+  export const help = () => (
+`**Pinging**
+Returns the milliseconds it takes for the bot to
+communicate with the Discord API. It requires no
+arguments.
+\`\`\`;ping\`\`\``
+  );
+
+  export const ping = (ping: number) => (
+`\u2002:clock3: ${ping}ms`
+  );
 }
 export = PingCmd;
