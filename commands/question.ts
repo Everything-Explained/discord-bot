fc99c45ae3e923e8d52e5df11cfa1b9cdd0f3b34
Jaeiya <exanix@gmail.com>
2020-10-20 17:14:30 -0700

    cmd(question): Update SAI references and Implement Strings  namespace

--- commands/question.ts
+++ commands/question.ts
@@ -1,42 +1,22 @@
 import Bot, { message_levels } from '../bot';
 import { Command } from '../command';
 import axios from 'axios';
-import { RepErrorCode, RepoItem } from '@noumenae/sai/dist/database/repository';
+import { InqErrorCode, Inquiry, InquiryManager } from '@noumenae/sai/dist/database/inquiry_manager';
 import { saiErrorResponses } from '../constants';
 
-class QuestionCmd extends Command {
 
-  /** From urlregex.com */
+
+
+class QuestionCmd extends Command {
+  /** URL RegEx from http://urlregex.com */
   private _urlEx = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w\-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)/
 
-
-  get help() {
-    return (
-`**Add Question**
-Checks that the \`<url>\` argument is a valid URL. If the
-URL is valid, then the bot will try to retrieve an Item
-Document resource from the URL and add it to its database.
-\`\`\`;question <url>\`\`\`
-**Find Question**
-Checks that the \`<question>\` argument is a valid question
-and if it is, tries to find that question in the database.
-\`\`\`;question <question>\`\`\`
-**List**
-Lists all saved questions and groups them together based
-on the document they came from.
-\`\`\`;question list\`\`\`
-**Question Document**
-Generates a raw Item Document from the specified \`<question>\`
-if it exists in the database.
-\`\`\`;question doc <question>\`\`\``
-    );
-  }
+  get help() { return Strings.getHelp(); }
 
 
   constructor(bot: Bot) {
     super(['question','qstn', 'q'], Bot.Role.Admin, bot);
   }
-
 
 
   _instructions(arg: string, ...args: string[]) {
@@ -49,50 +29,43 @@ if it exists in the database.
 
 
   private async _processURL(url: string) {
-    const [err, doc] = await this._getQuestionDoc(url);
+    const [errMsg, doc] = await this._getQuestionDoc(url);
+    // Don't need to clutter discord with file uploads
     this._bot.curMsg.delete()
     ;
-    if (err) {
-      return !this._bot.sendHighMsg(
-        `An Error occurred while interacting with the URL:\n\`${err}\``
-      );
-    }
+    if (errMsg) return (
+      this._bot.sendHighMsg(Strings.getUrlError(errMsg))
+    );
     this._parseQuestion(doc);
   }
 
 
   private _findQuestion(question: string) {
-    const item = this._bot.sai.ask(question);
-    if (item == RepErrorCode.Question) {
-      return this._bot.sendMedMsg(
-        'You entered an **Invalid** `URL` or `Question`'
-      );
-    }
-    if (!item) {
-      return this._bot.sendMedMsg(
-        `Sorry, I couldn't find that question in my database.`
-      );
-    }
+    const item = this._tryGetQuestion(question);
+    if (!item) return;
     this._sendQuestionDetails(item);
   }
 
 
   private _getRawDoc(question: string) {
-    const item = this._bot.sai.ask(question);
-    if(item == RepErrorCode.Question) {
-      return this._bot.sendMedMsg(
-        'That\'s an invalid question.'
-      );
-    }
-    if (!item) {
-      return this._bot.sendMedMsg(
-        `Sorry, I couldn't find that question in my database.`
-      );
-    }
+    const item = this._tryGetQuestion(question);
+    if (!item) return;
     this._bot.sendLowMsg(
-      `\`\`\`${this._bot.sai.repository.toItemDoc(item)}\`\`\``,
-      'Item Document',
+      `\`\`\`${this._bot.sai.inquiryManager.getInquiryDocFrom(item)}\`\`\``,
+      'Inquiry Document',
     );
+  }
+
+
+  private _tryGetQuestion(question: string) {
+    const item = this._bot.sai.ask(question);
+    if (item == InqErrorCode.Question) return (
+      this._bot.sendMedMsg(Strings.getInvalidQuestion())
+    );
+    if (!item) return (
+      this._bot.sendMedMsg(Strings.getQuestionNotFound())
+    );
+    return item;
   }
 
 
@@ -109,8 +82,8 @@ if it exists in the database.
   }
 
 
-  private _sendQuestionDetails(item: RepoItem) {
-    const questions = this._bot.sai.repository.questionsFromItem(item);
+  private _sendQuestionDetails(item: Inquiry) {
+    const questions = this._bot.sai.inquiryManager.getQuestionsFrom(item);
     this._bot.sendMsg(
       `\`\`\`editId: ${item.ids[0]}\`\`\`\n` +
       `Questions:${this._createQuestionList(questions)}\n` +
@@ -136,7 +109,7 @@ if it exists in the database.
 
 
   private _parseQuestion(doc: string) {
-    this._bot.sai.addQuestion(doc)
+    this._bot.sai.addInquiry(doc)
       .then(item => {
         const titleWord = item.dateCreated < item.dateEdited ? 'Edited' : 'Added';
         this._sendQuestionDetails(item);
@@ -146,7 +119,7 @@ if it exists in the database.
   }
 
 
-  private _catchParseError(err: RepErrorCode|NodeJS.ErrnoException) {
+  private _catchParseError(err: InqErrorCode|NodeJS.ErrnoException) {
     if (typeof err == 'number') {
       const errMsg = saiErrorResponses[err];
       if (!errMsg) {
@@ -162,5 +135,40 @@ if it exists in the database.
     );
   }
 
+}
+
+
+namespace Strings {
+  export const getHelp = () => (
+`**Add Question**
+Checks that the \`<url>\` argument is a valid URL. If the
+URL is valid, then the bot will try to retrieve an Item
+Document resource from the URL and add it to its database.
+\`\`\`;question <url>\`\`\`
+**Find Question**
+Checks that the \`<question>\` argument is a valid question
+and if it is, tries to find that question in the database.
+\`\`\`;question <question>\`\`\`
+**List**
+Lists all saved questions and groups them together based
+on the document they came from.
+\`\`\`;question list\`\`\`
+**Question Document**
+Generates a raw Item Document from the specified \`<question>\`
+if it exists in the database.
+\`\`\`;question doc <question>\`\`\``
+  );
+
+  export const getUrlError = (err: string) => (
+`An Error occurred while interacting with the URL:\n\`${err}\``
+  );
+
+  export const getInvalidQuestion = () => (
+'You entered an **Invalid** `Question`'
+  );
+
+  export const getQuestionNotFound = () => (
+`Sorry, I couldn't find that question in my database.`
+  );
 }
 export = QuestionCmd;
